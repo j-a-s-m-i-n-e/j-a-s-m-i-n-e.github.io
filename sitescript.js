df17289f6f657c3107a6e9237de95b408efc1b2c
j-a-s-m-i-n-e <53236301+j-a-s-m-i-n-e@users.noreply.github.com>
2019-07-23 15:44:14 -0400

    Add files via upload

---
+++ sitescript.js
@@ -0,0 +1,35 @@
+function welcomeAlert() {
+  alert("Welcome to my website!");
+  alert("Navigate using the links at the top of the page.");
+}
+
+function lightAlt() {
+  alert(document.getElementById("lightbulbID").alt)
+}
+
+function flowerAlt() {
+  alert(document.getElementById("flowerID").alt)
+}
+
+function purpleplantAlt() {
+  alert(document.getElementById("purpleplantID").alt)
+}
+
+function sunsetAlt() {
+  alert(document.getElementById("sunsetID").alt)
+}
+
+/*
+function headerColorChange() {
+  document.bgColor = rgb(145, 0, 111);
+}
+
+function OGheaderColor() {
+  document.bgColor = rgb(255, 177, 0);
+}
+
+function headerChange() {
+var x = document.getElementsByClassName("headerClass");
+document.bgColor = rgb(145, 0, 111)
+}
+*/
